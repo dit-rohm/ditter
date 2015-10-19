2473171a870a40bd7efa60f2f44f9b7de216352e
Eshin Kunishima <omochi@luna.miko.im>
2015-10-19 22:33:01 +0900

    Add sample config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+/* SQLite
+define('DSN', 'sqlite:ditter.sq3');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+*/
+
+/* MySQL */
+define('DSN', 'mysql:host=localhost;dbname=ditter;charset=utf8');
+define('DB_USER', 'ditter');
+define('DB_PASSWORD', '');
